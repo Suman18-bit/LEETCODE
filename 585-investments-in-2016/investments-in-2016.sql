5400343f854c4314da46403201932a5519dabf39
Suman Seth <sethn533@gmail.com>
2026-09-19 17:46:08 +0530

    Time: 708 ms (33.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH SameTIV2015 AS (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+),
+UniqueCity AS (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM SameTIV2015)
+  AND (lat, lon) IN (SELECT lat, lon FROM UniqueCity);
